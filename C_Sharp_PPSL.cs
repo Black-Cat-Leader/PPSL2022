5bccedd1a367c6a057334cb0604d9fe137a6500d
Black-Cat-Leader <84964377+Black-Cat-Leader@users.noreply.github.com>
2022-01-25 18:42:16 -0500

    Init

---
+++ C_Sharp_PPSL.cs
@@ -0,0 +1,19 @@
+//Class: CSE 1321L
+//Section: #01 C#
+//Term: Spring 2022
+//Instructor: Yamini Hanisha Talluri
+//Name: Thea Buckler
+//Lab#: Lab2C
+
+using System;
+
+namespace Lab2C
+{
+    class Lab2C
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
