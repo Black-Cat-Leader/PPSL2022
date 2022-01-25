65d047d9c665c2d830a73c6388d6260f1c2f8337
Black-Cat-Leader <84964377+Black-Cat-Leader@users.noreply.github.com>
2022-01-25 18:58:11 -0500

    1

--- PPSL2022/Program.cs
+++ PPSL2022/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Class: CSE 1321L
+//Section: #01 C#
+//Term: Spring 2022
+//Instructor: Yamini Hanisha Talluri
+//Name: Thea Buckler
+//Lab#: Lab2C
+
+using System;
 
 namespace PPSL2022
 {
